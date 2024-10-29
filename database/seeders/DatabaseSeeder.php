3926733208ddd31936c0dae42b29143002d8e109
bhinti <bintikhusnul69@gmail.com>
2024-10-29 14:06:20 +0700

    "Updated DatabaseSeeder to call AuthorSeeder and BookSeeder, and added route for books index"

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $this->call([
+            AuthorSeeder::class,
+            BookSeeder::class,
+        ]);
 
+        
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
