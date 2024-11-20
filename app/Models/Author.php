b2707d761b20ce472104d9bec6044d7ee49798bc
bhinti <bintikhusnul69@gmail.com>
2024-11-20 10:15:13 +0700

    feat: :sparkles:

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Validation\Validator;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,5 +13,34 @@ class Author extends Model
     publIc function books()
     {
         return $this->hasMany('Book::class','author_id');
+    }
+    public static function rules($process)
+    {
+        if ($process == 'insert') {
+            return [
+                'name' => 'required|string|max:225',
+                'birthdate' => 'required|date',
+            ];
+        } elseif ($process == 'update') {
+            return [
+                'name' => 'required|string|max:225',
+                'birthdate' => 'required|date',
+            ];
+        }
+    }
+    public static function customValidation(Validator $validator)
+    {
+        $customAttributes = [
+            'name' => 'Nama',
+            'birthdate' => 'Tanggal Lahir',
+        ];
+
+        $validator->addReplacer('required', function ($message, $attribute, $rule, $parameters) use ($customAttributes) {
+            return str_replace(':attribute', $customAttributes[$attribute], ':attribute harus diisi.');
+        });
+
+        $validator->addReplacer('date', function ($message, $attribute, $rule, $parameters) use ($customAttributes) {
+            return str_replace(':attribute', $customAttributes[$attribute], ':attribute harus berupa tanggal.');
+        });
     }
 }
